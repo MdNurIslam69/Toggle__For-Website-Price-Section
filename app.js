dfc2f5145d6f7298c6a32d7febe1c9358f4d5df4
Md Nur Islam <nurislamimran23@gmail.com>
2025-02-10 01:33:09 +0600

    argument change

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-let t = 1;
+let t = true;
 let toggle = document.getElementById("toggle");
 
 toggle.addEventListener("click", () => {
-  if (t == 1) {
-    t = 2;
+  if (t == true) {
+    t = false;
     toggle.classList.remove("left-0");
     toggle.classList.add("right-0");
   } else {
-    t = 1;
+    t = true;
     toggle.classList.remove("right-0");
     toggle.classList.add("left-0");
   }
 
   let price = document.getElementsByClassName("price");
 
-  if (t == 1) {
+  if (t == true) {
     price[0].innerHTML = "$99/m";
     price[1].innerHTML = "$199/m";
     price[2].innerHTML = "$299/m";
